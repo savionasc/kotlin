9ef2ae88e5be7a861729fd1f331a06e2eafae51e
Savio Nascimento <savio.nascimento@beegol.com>
2021-11-06 16:24:22 -0300

    Adicionando Inner Class e private set

--- src/main/kotlin/dio/Pessoa.kt
+++ src/main/kotlin/dio/Pessoa.kt
@@ -3,6 +3,12 @@ package dio
 class Pessoa {
     var nome: String = "Top"
     var cpf: String = "210.441.393-15"
+
+    private set
+
+    inner class Endereco {
+        var rua: String = "Maria Baviera"
+    }
 }
 
 fun main(){
@@ -10,4 +16,5 @@ fun main(){
 
     println(fulano.nome)
     println(fulano.cpf)
+    println(fulano.Endereco().rua)
 }
